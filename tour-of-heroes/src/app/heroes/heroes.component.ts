4839b795784476c2f66812d384383c82e6c95c6b
giovanni.frison <giovanni.frison@alten.it>
2024-01-03 13:01:10 +0100

    separate component HeroDetail from Hero; parent-child property binding

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {NgFor, NgIf, UpperCasePipe} from "@angular/common";
 import {Hero} from "../hero";
+import {HeroDetailComponent} from "../hero-detail/hero-detail.component";
 import {HEROES} from "../mock-heroes";
 import {FormsModule} from "@angular/forms";
 
@@ -10,13 +11,12 @@ import {FormsModule} from "@angular/forms";
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
-  imports: [NgFor, NgIf, UpperCasePipe, FormsModule]
+  imports: [NgFor, NgIf, UpperCasePipe, FormsModule, HeroDetailComponent]
 })
 export class HeroesComponent {
   heroes = HEROES;
-  protected readonly onselect = onselect;
 
-  selectedHero ?: Hero;
+  selectedHero !: Hero;
   onSelect(hero: Hero) {
     this.selectedHero = hero;
   }
