ba7d28bfa08f9ae3d6e735646bcdd4e0ede8cdda
giovanni.frison <giovanni.frison@alten.it>
2024-01-03 15:00:28 +0100

    created a service that fetch data asynchronously using rxJs

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -4,6 +4,7 @@ import {Hero} from "../hero";
 import {HeroDetailComponent} from "../hero-detail/hero-detail.component";
 import {HEROES} from "../mock-heroes";
 import {FormsModule} from "@angular/forms";
+import {HeroService} from "../hero.service";
 
 
 @Component({
@@ -14,10 +15,23 @@ import {FormsModule} from "@angular/forms";
   imports: [NgFor, NgIf, UpperCasePipe, FormsModule, HeroDetailComponent]
 })
 export class HeroesComponent {
-  heroes = HEROES;
+
+  constructor(private heroService: HeroService) {
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  heroes: Hero[] = [];
 
   selectedHero !: Hero;
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 }
